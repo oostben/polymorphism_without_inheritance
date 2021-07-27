0baf9449d7c507d975b47ec6d03a1698e3d44be7
oostben <oostben@umich.edu>
2021-07-27 01:59:02 -0400

    final transformation to add in support for vectors; done

--- library.cpp
+++ library.cpp
@@ -8,14 +8,22 @@ void draw(const int& x, ostream& out, size_t position)
     out << string(position, ' ') << x << endl;
 }
 
-//heap allocate int_model_t
+//add in a way to draw a string
+void draw(const string& x, ostream& out, size_t position)
+{
+    out << string(position, ' ') << x << endl;
+}
+
 class object_t 
 {
     public:
-        //pass ctor arguments by value and move them into place to use our own 
+        //add another constructor for strings:
+        object_t(string x): self_(make_unique<string_model_t>(move(x))) { }
+
         object_t(int x): self_(make_unique<int_model_t>(move(x))) { }
 
-        object_t(const object_t& x): self_(make_unique<int_model_t>(*x.self_)) { }
+        //virtualize copy constructor
+        object_t(const object_t& x): self_(x.self_->copy_()) { }
 
         object_t(object_t&& x) noexcept = default;
 
@@ -32,17 +40,53 @@ class object_t
         }
     
     private:
-        struct int_model_t
+        //define concept base class, so we can store whatever we like:
+        struct concept_t
+        {
+            virtual ~concept_t() = default;
+            //virtualize copy:
+            virtual unique_ptr<concept_t> copy_() const = 0;
+            //virualize the draw function:
+            virtual void draw_(ostream&, size_t) const = 0;
+        };
+
+        struct int_model_t final : concept_t
         {
             int_model_t(int x): data_(move(x)) { }
-            void draw_(ostream& out,size_t position) const
+
+            //virtualize copy:
+            unique_ptr<concept_t> copy_() const override
+            {
+                return make_unique<int_model_t>(*this);
+            }
+
+            void draw_(ostream& out,size_t position) const override //signals to the compiler that this must overide the virtual in concept_t
             {
                 draw(data_, out, position);
             }
             int data_;
         };
 
-        unique_ptr<int_model_t> self_;
+        //add in string support
+        struct string_model_t final : concept_t
+        {
+            string_model_t(string x): data_(move(x)) { }
+            
+            //virtualize copy:
+            unique_ptr<concept_t> copy_() const override
+            {
+                return make_unique<string_model_t>(*this);
+            }
+
+            void draw_(ostream& out,size_t position) const override
+            {
+                draw(data_, out, position);
+            }
+            string data_;
+        };
+
+        //now store a concept for a drawable object
+        unique_ptr<concept_t> self_;
 };
 
 using document_t = vector<object_t>;
--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main() 
 {
+    //now we can do this!:
     document_t document;
     document.emplace_back(1);
-    document.emplace_back(2);
+    document.emplace_back(string("Hello"));
     document.emplace_back(3);
-    document.emplace_back(4);
+    document.emplace_back(5);
     cout << "done building vector" << endl;
     std::reverse(document.begin(), document.end());
 
