a569f0665e4ee94fdda243af0ba57c994bc1ae01
oostben <oostben@umich.edu>
2021-07-26 23:06:08 -0400

    traditional polymorophism implemented and why its bad --> see .txt

--- library.cpp
+++ library.cpp
@@ -3,19 +3,23 @@ using namespace std;
 #include <string>
 #include <vector>
 
-using object_t = int;
-
-void draw(const object_t& x, ostream& out, size_t position)
+//traditional polymorphism with pure virtual class and vtable, and shared pointers to manage memory scope
+class object_t 
 {
-    out << string(position, ' ') << x << endl;
-}
+    public:
+        virtual ~object_t() { }
+        virtual void draw(ostream&, size_t) const = 0;
+};
 
-using document_t = vector<object_t>;
+using document_t = vector<shared_ptr<object_t>>;
 
 void draw(const document_t& x, ostream& out, size_t position)
 {
     out << position << string(position, ' ') << "<document>" << endl;
-    for (const auto & e: x) draw(e,out,position + 2);
+    for (const auto & e: x)
+    {
+        e->draw(out,position + 2);
+    } 
     out << position <<  string(position, ' ') << "</document>" << endl;
 
 }
--- main.cpp
+++ main.cpp
@@ -2,14 +2,31 @@
 #include "library.cpp"
 using namespace std;
 
+
+//implement the traditional polymorphism wrapper sub class derived from object_t, so I can put a certain type into the document
+
+class my_class_t : public object_t 
+{
+    virtual void draw(ostream& out, size_t position) const override 
+    {
+        out << string(position, ' ') << "my_class_t" << endl;
+    }
+
+};
+
 int main() 
 {
     document_t document;
-    document.emplace_back(0);
-    document.emplace_back(2);
-    document.emplace_back(1);
-    document.emplace_back(3);
-    document.emplace_back(4);
+    //update to use emplace back to call the constructor directly and avoid the extra copy for my new type
+    document.emplace_back(new my_class_t()); //this line could result in a leak because the object could be constructed, but not yet assigned to a smart pointer
+                                            //if say the growing of the vector threw an exception
+                                            //most people in c++ community will tell you dint call new directly
+                                            //instead call make_shared<my_class_t> to avoid this
+                                            //its also better becasue it allocates the shared pointer and the class in the same block so sames a little memory and a bit of time
+    document.emplace_back(make_shared<my_class_t>());
+    document.emplace_back(make_shared<my_class_t>());
+    document.emplace_back(make_shared<my_class_t>());
+    document.emplace_back(make_shared<my_class_t>());
 
     draw(document,cout,0);
 }
